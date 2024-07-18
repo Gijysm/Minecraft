7781539fce6addedf1f904d62102614ada0b6e4c
popkagnoma23@hotmail.com <popkagnoma23@hotmail.com>
2024-07-18 15:59:09 +0300

    #0.300000000000000001

--- OpenGlGame2/Png_loading.cpp
+++ OpenGlGame2/Png_loading.cpp
@@ -13,14 +13,22 @@ GLuint png_load(const char* file, int* width, int* height, int* nrComponents) {
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
 
-    // Завантаження даних зображення в текстуру
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *width, *height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    GLenum format;
+    if (*nrComponents == 1)
+        format = GL_RED;
+    else if (*nrComponents == 3)
+        format = GL_RGB;
+    else if (*nrComponents == 4)
+        format = GL_RGBA;
 
-    // Встановлення параметрів текстури
+    // Завантаження даних зображення в текстуру
+    glTexImage2D(GL_TEXTURE_2D, 0, format, *width, *height, 0, format, GL_UNSIGNED_BYTE, image);
+
+    // Встановлення параметрів текстури для піксельного вигляду
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     // Генерація mipmaps
     glGenerateMipmap(GL_TEXTURE_2D);
